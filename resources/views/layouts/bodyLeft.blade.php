41ba810f1e77abff0aac89ce0d836a45c5a731b7
Mateus Arruda <mateusarruda@alu.ufc.br>
2018-05-31 15:04:00 -0300

    Template Usuario

---
+++ resources/views/layouts/bodyLeft.blade.php
@@ -0,0 +1,25 @@
+<div >
+  <div class="center-align">
+    <img class="responsive circle" src="{{asset('images/generic.png')}}" style="height:200px; margin-top:100px; " alt="">
+  </div>
+  <br><br>
+  <div class="card-panel teal center ">
+          <span class="white-text">
+            <h5>Programador</h5>
+          </span>
+        </div>
+
+  <div class="items-cv">
+    <i class="fa fa-phone fa-2x"></i> <span style="font-size:20px; font-weight: 300;">(88)996061495</span>
+  </div>
+  <div class="items-cv">
+    <i class="fa fa-envelope fa-2x"></i> <span style="font-size:20px; font-weight: 300;">mateusarruda232@hotmail.com</span>
+  </div>
+  <div class="items-cv">
+    <i class="fa fa-home fa-2x"></i> <span style="font-size:20px; font-weight: 300;">Jose Julio, 300</span>
+  </div>
+  <div class="items-cv">
+    <i class="fa fa-home fa-2x"></i> <span style="font-size:20px; font-weight: 300;">Jose Julio, 300</span>
+  </div>
+
+</div>
